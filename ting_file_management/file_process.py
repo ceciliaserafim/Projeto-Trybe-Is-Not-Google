9914feaa969d948d8ea01bede32593e03941b024
Cecília Serafim <ceciliaserafimti@gmail.com>
2023-06-13 14:37:17 -0300

    Adiciona requisito 3 completo

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,17 +1,20 @@
 from ting_file_management.file_management import txt_importer
+import sys
 
 
 def process(path_file, instance):
     for item in range(len(instance)):
-        if item == path_file:
+        if instance.search(item)["nome_do_arquivo"] == path_file:
             return None
 
     file_line = txt_importer(path_file)
     dict_data = {
-            "nome do arquivo": path_file,
+            "nome_do_arquivo": path_file,
             "qtd_linhas": len(file_line),
             "linhas_do_arquivo": file_line,
         }
+    instance.enqueue(dict_data)
+    return print(dict_data, file=sys.stdout)
 
 
 def remove(instance):
