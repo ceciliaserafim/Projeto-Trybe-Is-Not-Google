c3c65ec9bce33299dcf6a8ed08c279d6ee8bc1ba
Cecília Serafim <ceciliaserafimti@gmail.com>
2023-06-13 16:06:31 -0300

    Adiciona requisito 4 - remove

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -18,7 +18,12 @@ def process(path_file, instance):
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    if len(instance) == 0:
+        return print("Não há elementos")
+    else:
+        file = instance.dequeue()
+        path_file = file["nome_do_arquivo"]
+        print(f"Arquivo {path_file} removido com sucesso", file=sys.stdout)
 
 
 def file_metadata(instance, position):
