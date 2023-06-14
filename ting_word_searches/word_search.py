0cd73b949d5c375c34d516a9dc278c2e6bfffb0a
Cecília Serafim <ceciliaserafimti@gmail.com>
2023-06-14 12:30:22 -0300

    Adiciona requisito 7 - procura word

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,19 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+
+    list_result = []
+    for data in instance._list:
+        with open(data["nome_do_arquivo"], 'r') as file:
+            occurrence = []
+            for count_item, line in enumerate(file, start=1):
+                if word.lower() in line.lower():
+                    occurrence.append({"linha": count_item})
+            if occurrence:
+                list_result.append({
+                    "palavra": word,
+                    "arquivo": data["nome_do_arquivo"],
+                    "ocorrencias": occurrence,
+                })
+    return list_result
 
 
 def search_by_word(word, instance):
